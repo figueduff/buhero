d87002247d2dde76be936d2bb42738861bc165c8
figueduff <figueduff@hotmail.com>
2024-01-23 22:32:49 -0300

    produ

--- src/paginas/home.jsx
+++ src/paginas/home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <section>
-      <img src="./img/Home_header_transparente.webp" alt="" />
+      <img src="/img/Home_header_transparente.webp" alt="" />
     </section>
   )
 }
