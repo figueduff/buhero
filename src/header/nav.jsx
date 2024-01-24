96f2cabbb1eab002d317b0b77802160288b6c67f
figueduff <figueduff@hotmail.com>
2024-01-24 17:47:25 -0300

    update menu

--- src/header/nav.jsx
+++ src/header/nav.jsx
@@ -1,28 +1,33 @@
 import { Link } from 'react-router-dom';
 import './nav.css';
+import DropDown from './dropdown';
 
 const Nav = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/fernet">FernetFernet</Link>
-        </li>
-        <li id='experiencia'>
-          <Link to="/experiencia">ExperienciaExperiencia</Link>
-        </li>
-        <li id='home'>
-          <Link to="/">HomeHome</Link>
-        </li>
-        <li id='contacto'>
-          <Link to="/contacto">ContactoContacto</Link>
-        </li>
-        <li >
-          <p>Comprar</p>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/fernet">FernetFernet</Link>
+          </li>
+          <li id='experiencia'>
+            <Link to="/experiencia">ExperienciaExperiencia</Link>
+          </li>
+          <li id='home'>
+            <Link to="/">HomeHome</Link>
+          </li>
+          <li id='contacto'>
+            <Link to="/contacto">ContactoContacto</Link>
+          </li>
+          <li >
+            <p>Comprar</p>
+          </li>
+        </ul>
+      </nav>
+      <DropDown/>
+    </>
   );
 };
 
 export default Nav;
+
--- src/paginas/home.jsx
+++ src/paginas/home.jsx
@@ -1,26 +1,31 @@
 import PropTypes from 'prop-types';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './home.css';
 import Nav from '../header/nav';
 
 function Home({ pagina }) {
   const navigate = useNavigate();
+  const location = useLocation();
+  const pathname = location.pathname;
 
   const handleClick = (destino) => {
     navigate(`/${destino}`);
 }
 
+const contienePagina = ["/fernet", "/botanicos", "/maceracion", "/reposo", "/cata"];
+
   return (
     <section className="contenedor-imagen">
       <Nav />
       <img src={pagina} alt="Buhero" />
+      {contienePagina.includes(pathname) && (
+        <>
       <button className="botonBotanica" onClick={() => handleClick("botanicos")}>fernete</button>
-
       <button className="botonMaceracion" onClick={() => handleClick("maceracion")}>experiencia</button>
-
       <button className="botonReposo" onClick={() => handleClick("reposo")}>reposorepo</button>
-
       <button className="botonCata" onClick={() => handleClick("cata")}>catacataca</button>
+      </>
+      )}
     </section>
   )
 }
