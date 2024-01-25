18c9c306397564de7f79e5cabfb8c1e768143043
figueduff <figueduff@hotmail.com>
2024-01-25 03:50:51 -0300

    buhero final

--- src/header/navMobile.jsx
+++ src/header/navMobile.jsx
@@ -1,6 +1,16 @@
 import './navMobile.css'
+import { Link } from 'react-router-dom';
 
 function navMobile() {
+    const closeOffcanvas = () => {
+        const offcanvas = document.getElementById('offcanvasDarkNavbar');
+        if (offcanvas.classList.contains('show')) {
+          offcanvas.classList.remove('show');
+          document.body.classList.remove('offcanvas-shown');
+          document.body.style.paddingRight = '0';
+        }
+      };
+
     return (
         <div>
             <nav className="navbar navbar-dark fixed-top navbar-expand-xs">
@@ -11,6 +21,7 @@ function navMobile() {
                             outline: 'none',
                             background: 'transparent',
                         }} >
+                        {/* <span className="navbar-toggler-icon" style="opacity: 0.5;"></span> */}
                     </button>
                     <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
                         <div className="offcanvas-header">
@@ -22,13 +33,16 @@ function navMobile() {
                                     <a className="nav-link active" aria-current="page" href="/">Home</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/fernet">Fernet</a>
+                                    {/* <a className="nav-link" href="/fernet">Fernet</a> */}
+                                    <Link className="nav-link" to="/fernet" onClick={closeOffcanvas}>Fernet</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/experiencia">Experiencia Buhero</a>
+                                    {/* <a className="nav-link" href="/experiencia">Experiencia Buhero</a> */}
+                                    <Link className="nav-link" to="/experiencia" onClick={closeOffcanvas}>Experiencia</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/contacto">Contacto</a>
+                                    {/* <a className="nav-link" href="/contacto">Contacto</a> */}
+                                    <Link className="nav-link" to="/contacto" onClick={closeOffcanvas}>Contacto</Link>
                                 </li>
 
                             </ul>
