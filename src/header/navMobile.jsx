329e91f0b76fa95814628281220ac3496c879584
figueduff <figueduff@hotmail.com>
2024-01-25 00:21:44 -0300

    ok_buhero

--- src/header/navMobile.jsx
+++ src/header/navMobile.jsx
@@ -6,10 +6,11 @@ function navMobile() {
             <nav className="navbar navbar-dark fixed-top navbar-expand-xs">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar"
-                    style={{ width: '50px', height: '50px',border: 'none',
-                    outline: 'none', 
-                    background: 'transparent', }} >
-                        {/* <span className="navbar-toggler-icon" style="opacity: 0.5;"></span> */}
+                        style={{
+                            width: '50px', height: '50px', border: 'none',
+                            outline: 'none',
+                            background: 'transparent',
+                        }} >
                     </button>
                     <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
                         <div className="offcanvas-header">
